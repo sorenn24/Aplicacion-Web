80ccca4e5f298456dfe78cacbf3b79ffa0bea19c
sorenn24 <140275920+sorenn24@users.noreply.github.com>
2025-10-23 11:10:46 -0600

    Add files via upload

---
+++ codigos/integradora/home/home.js
@@ -0,0 +1,11 @@
+// Configuración de Tailwind personalizada
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6366F1',
+        secondary: '#EC4899'
+      }
+    }
+  }
+};
