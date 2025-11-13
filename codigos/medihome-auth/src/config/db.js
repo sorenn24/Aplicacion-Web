677a359ee50476f24098beae7200949f37733a82
sorenn24 <140275920+sorenn24@users.noreply.github.com>
2025-11-13 15:25:59 -0600

    Add files via upload

--- codigos/medihome-auth/src/config/db.js
+++ codigos/medihome-auth/src/config/db.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose");
 
+async function connectDB(uri) {
+  mongoose.set("strictQuery", true);
+  await mongoose.connect(uri);
+  console.log("✅ MongoDB conectado");
+}
+
+module.exports = { connectDB };
